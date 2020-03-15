3634c8ef00084cc8650d77123ae789c3aac7701a
Preetam D'Souza <preetamjdsouza@gmail.com>
2020-03-14 22:00:38 -0400

    fix(api): Specify $PORT env var for server

--- api/server.go
+++ api/server.go
@@ -3,9 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/cases/", CasesHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
