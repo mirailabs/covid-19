8a8c0f628e65bab1928ee119c771201cdf70cda7
Preetam D'Souza <preetamjdsouza@gmail.com>
2020-05-05 13:04:46 -0400

    feat(api): Allow cross origin requests from *

--- api/cases.go
+++ api/cases.go
@@ -62,5 +62,6 @@ func CasesHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(json)
 }
